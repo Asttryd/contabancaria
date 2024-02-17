ee6b82b5a1f2642a1ab63b77bc05fa6370e17fc0
asttryd <asttrydp@gmail.com>
2024-02-16 21:42:49 -0300

    Primeiro commit: classes Main e Cores foram criadas e testadas

---
+++ src/conta/Menu.java
@@ -0,0 +1,12 @@
+package conta;
+
+import conta.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println(Cores.Cores());
+
+	}
+
+}
---
+++ src/conta/util/Cores.java
@@ -0,0 +1,10 @@
+package conta.util;
+
+public class Cores {
+
+	public static String Cores () {
+		// TODO Auto-generated method stub
+		return "Hello World";
+	}
+
+}
